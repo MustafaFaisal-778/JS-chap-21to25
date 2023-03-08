a692bc11737725a13678928a7463286629ed1dce
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-03-09 00:13:24 +0500

    Add files via upload

---
+++ JS chap 21 to 25/app.js
@@ -0,0 +1,144 @@
+//Question#1
+// var firstName=prompt("enter first name ")
+// var lastName=prompt("enter last name ")
+// var fullName= firstName+" "+ lastName
+// console.log("Hello "+fullName)
+
+// Question2
+// var mobileName=prompt("enter your favorite smartphone model")
+// document.write("<h1>"+mobileName+"</h1>")
+//  var length=mobileName.length
+// document.write(length) 
+
+// Question#3
+// var input=("pakistani")
+// var index=input.indexOf("n")
+// console.log(input)
+// console.log(index)
+
+// Question#4
+// var input=("hello world")
+// var index=input.lastIndexOf("l")
+// console.log(input)
+// console.log(index)
+
+// Question#5
+// var input=("pakistani")
+// var index=input.charAt("3")
+// console.log(input)
+// console.log(index)
+
+// Question#6
+// var firstName=prompt("enter first name ")
+// var lastName=prompt("enter last name ")
+// var result = firstName.concat("  ",lastName);
+// console.log(result)
+
+// Question#7
+// var input=("hydrabad")
+// console.log(input)
+// var replace=input.replace("hydra","islama")
+// console.log(replace)
+
+// Question#8
+// var message = "Ali and Sami are best friends. They play cricket and football together";
+// var result =message.replaceAll("and","&")
+// console.log(result)
+
+// Question#9
+// var number=("472")
+
+// console.log(typeof(number))
+// var a=parseInt(number)
+// console.log(typeof(a))
+
+// Question#10
+// var input=prompt("write")
+// console.log(input)
+// a=input.toUpperCase()
+// console.log(a)
+
+// Question#11
+// var input = prompt("enter anything:");
+// var words = input.split(" ");
+
+// for (i = 0; i < words.length; i++) {
+//   var word = words[i];
+//   words[i] = word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+// }
+
+
+// var result = words.join(" ");
+// console.log(result);
+
+// Question#12
+// var num = 35.36;
+// var str = num.toString();
+
+// var newStr = "";
+// for (var i = 0; i < str.length; i++) {
+//   if (str[i] !== ".") {
+//     newStr += str[i];
+//   }
+// }
+
+// document.write(newStr);
+
+// Question#13
+// var input=prompt("enter username")
+// var check=input.split("")
+
+// for(i=0;i<check.length;i++){
+//     // console.log(check[i])
+
+//     if(check[i]=="!" || check[i]=="," || check[i]=="." || check[i]=="@"){
+//         alert("invalid syntax try again")
+
+//     }
+   
+// }
+
+// Question#14
+// var a = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var search=prompt("search any thing in array ")
+
+// for(i=0;i<a.length;i++){
+
+//   if (a[i]==search){
+    
+//      console.log(a.indexOf(a[i]))
+    
+
+// }
+
+// }
+
+// Question#16
+// var university = "University of Karachi";
+// a=university.split("")
+// console.log(a)
+
+// Question#17
+// var input="pakistan"
+
+// console.log(input[input.length-1])
+
+// Question#18
+// var str = "The quick brown fox jumps over the lazy dog";
+// var count = 0;
+
+
+// var words = str.split(" ");
+
+// for (var i = 0; i < words.length; i++) {
+//   if (words[i].toLowerCase() === "the") {
+//     count++;
+//   }
+// }
+
+// console.log("The word 'the' appears " + count + " times in the string.");
+
+
+
+
+
